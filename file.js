f63e85a3c6b16aacad097c78f8c6f6501b9c0f74
pankajsingh1594 <110449619+pankajsingh1594@users.noreply.github.com>
2022-10-15 13:29:44 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,16 @@
+/* LOADING THE DOCUMENT */
+$(document).ready(function () {
+
+    /* USING MOUSE HOVER EVENT FOR CHANGING THE LOGO */
+    $("#logo").hover(function(){
+        $("#logo").attr("src","/brandstar-images/brandstar-Logo-2.png");
+    },function(){
+        $("#logo").attr("src","/brandstar-images/brandstar-Logo-1.png");
+    });
+
+    /* HAMBURGER TOGGLE */
+    $(".hamburger_menu").on("click", function () {
+        $("#nav_container").slideToggle();
+        $(".hamburger_menu").toggleClass("toggle");
+    });
+});
